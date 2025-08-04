46241319085a7e9f8e852ffcc02d5e25c20136b7
Shashankshekhar0007 <shashankshekhar564@gmail.com>
2025-08-04 11:05:23 +0530

    Upto DB Connect

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const http = require('http');
+const express = require('express');
+const connectDB = require('./config/db');
+const app = express();
+const registerRoute = require('./routes/registerRoute');
+const cors = require('cors');
+app.use(cors());
+
+
+connectDB();
+
+app.use('/register', registerRoute);
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+
+});
